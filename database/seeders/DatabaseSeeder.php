f7005db2d45c383e7ecee2cae25f368d7c35ddf5
Hung <hungbiuu@gmail.com>
2025-11-11 20:03:16 +0700

    Đánh giá, số sao trung bình, Thêm đánh giá, gợi ý sản phẩm tương tự cho chi tiết sản phẩ

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
             CategorySeeder::class,
             ProductSeeder::class,
             ProductGallerySeeder::class,
+            ReviewsSeeder::class,
         ]);
     }
 }
