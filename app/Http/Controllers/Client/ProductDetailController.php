221165fe790449d59c25ee6a4e11fe18883dec3c
LngThB2203 <lngthb220305@gmail.com>
2025-11-14 14:51:17 +0700

    Sắp xếp Controller

---
+++ app/Http/Controllers/Client/ProductDetailController.php
@@ -0,0 +1,32 @@
+<?php
+namespace App\Http\Controllers\Client;
+
+use App\Http\Controllers\Controller;
+use App\Models\Product;
+
+class ProductDetailController extends Controller
+{
+    public function show(string $slug)
+    {
+        $product = Product::with([
+            'galleries',
+            'category',
+            'reviews.user',
+            'variants',
+        ])->where('slug', $slug)->firstOrFail();
+
+        $reviews = $product->reviews()->where('status', 1)->latest()->get();
+
+        $relatedProducts = Product::where('category_id', $product->category_id)
+            ->where('id', '!=', $product->id)
+            ->take(6)
+            ->get();
+
+        return view('client.product', [
+            'product'         => $product,
+            'galleries'       => $product->galleries,
+            'reviews'         => $reviews,
+            'relatedProducts' => $relatedProducts,
+        ]);
+    }
+}
