b368993e4f018bfc63f1b5e0daee572533570893
My Name <namtv2005>
2025-11-26 16:39:37 +0700

    Chỉnh sửa layout trang tin tức

--- app/Http/Controllers/ClientBlogController.php
+++ app/Http/Controllers/ClientBlogController.php
@@ -2,35 +2,34 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
-use Illuminate\Http\Request; // đúng Request
+use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-
+use Carbon\Carbon;
 class ClientBlogController extends Controller
 {
-    // Trang danh sách blog + tìm kiếm
     public function index(Request $request)
     {
-        $query = Post::query();
-
-        // Lọc theo từ khóa search
+         $query = Post::query();
         if ($request->has('search') && $request->search != '') {
             $query->where('title', 'like', '%' . $request->search . '%');
         }
-
-        // Lấy 6 bài viết/trang, mới nhất
         $posts = $query->latest()->paginate(6);
-
-        return view('client.blog', compact('posts'));
+         $latestPosts = Post::whereDate('created_at', '>=', Carbon::now()->subDays(7))
+                           ->latest()
+                           ->limit(5)
+                           ->get();
+        return view('client.blog', compact('posts', 'latestPosts'));
     }
 
-    // Trang chi tiết bài viết
     public function show($slug)
     {
         $post = Post::where('slug', $slug)->firstOrFail();
 
-        // Optional: lấy 4 bài viết liên quan
         $relatedPosts = Post::where('id', '!=', $post->id)->latest()->take(4)->get();
-
-        return view('client.blog-details', compact('post', 'relatedPosts'));
+       $latestPosts = Post::whereDate('created_at', '>=', Carbon::now()->subDays(7))
+                           ->latest()
+                           ->limit(5)
+                           ->get();
+        return view('client.blog-details', compact('post', 'latestPosts', 'relatedPosts'));
     }
 }
